bf1a60a052d842e437a20af82b613b6307f1b850
Hossam Mourad <ihos4m@gmail.com>
2018-10-08 22:28:40 +0400

    remove redundant code

--- config/index.js
+++
@@ -1,5 +0,0 @@
-export default {
-  websocket: {
-    baseURL: 'http://localhost:3000'
-  }
-};
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-import openSocket from 'socket.io-client';
-import config from '../../../config';
 
 export default class NotFound extends Component {
-  componentDidMount() {
-    openSocket(config.websocket.baseURL);
-  }
-
   render() {
     return (
       <div>
